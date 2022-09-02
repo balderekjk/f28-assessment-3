1278c8a31ff1125ef0be59033e28e7603a040a40
Derek Bal <bal.derek.j@gmail.com>
2022-09-02 06:50:36 -1000

    Part 4 done

---
+++ prof.js
@@ -0,0 +1,15 @@
+const color = document.querySelector('#color');
+const place = document.querySelector('#place');
+const ritual = document.querySelector('#ritual');
+
+color.addEventListener('click', () => {
+  alert('Sunlit Green');
+});
+
+place.addEventListener('click', () => {
+  alert("Waihe'e Wetlands");
+});
+
+ritual.addEventListener('click', () => {
+  alert('Listening to instrumental music for calming/waking effects');
+});
